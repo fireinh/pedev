f295f3345d0759de4b73e3706f5516be98651c8f
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    beryllium: Enforce common device & vendor tree presence
    A compiled build has no chance of booting without the common device setup
    and the prebuilt binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
+$(call inherit-product, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
