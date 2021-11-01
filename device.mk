4e56e581b1274eb657ba991fc95571c27bba8221
YumeMichi <do4suki@gmail.com>
2021-11-01 18:45:56 +0600

    beryllium: overlay: Rename NoCutoutOverlay
    AOSP has its own NoCutoutOverlay
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Signed-off-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: Id22058851dba13bf3e667c1ff2f967d94803fafd

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    DeviceNoCutoutOverlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
