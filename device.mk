3ba5701e2a97752124acdd5cd6f4e32b3e9a4f2e
resist15 <souravhjagtap21@gmail.com>
2022-08-09 10:47:41 +0530

    beryllium: Cleanup Deprecated And Useless Flags

--- device.mk
+++ device.mk
@@ -9,10 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2246
-TARGET_SCREEN_WIDTH := 1080
-
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
