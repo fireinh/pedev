8d4af10a403e00f1a96722576c86f99268417652
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-08-26 17:22:09 +0600

    beryllium: PixelOS Bringup

---
+++ aosp_beryllium.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2018-2021 The PixelOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+# BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+
+# PRODUCT_BUILD_PROP_OVERRIDES += \
+#     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+#     PRODUCT_NAME="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
