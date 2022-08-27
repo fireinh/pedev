4975675edfc64c69e97b8a6a49f1e506b49c39c1
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-08-27 12:15:22 +0600

    beryllium: Enable quicktap
    Change-Id: Id45e46fc6a4994c15f494902bc31b991dde8bd63

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/aosp/config/common.mk)
 # Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_beryllium
