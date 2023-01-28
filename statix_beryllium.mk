99d427795e5b02375f751c6338474393edaceb12
sajidshahriar72543 <sazidshahriar39@gmail.com>
2023-01-28 16:19:34 +0000

    mkc

--- statix_beryllium.mk
+++ statix_beryllium.mk
@@ -7,7 +7,8 @@
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 # Flags
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -15,19 +16,21 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_USES_BLUR := true
+ENABLE_GAMETOOLS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := statix_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_SYSTEM_NAME := beryllium
 
-# BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
-# PRODUCT_BUILD_PROP_OVERRIDES += \
-#     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
-#     PRODUCT_PRODUCT="beryllium"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+     PRODUCT_PRODUCT="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
