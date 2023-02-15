57cf56b28345821289a61be0d39cc45971b87edd
gotenksIN <forumomkar@gmail.com>
2023-02-16 01:54:30 +0600

    beryllium: Bring back stock fingerprint
    - Needed for PIHooks
    
    Change-Id: I67e4f5e2d5194fcd94d027a69d81bef9dd8d7c68

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -24,10 +24,10 @@ PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_SYSTEM_NAME := beryllium
 
-# BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
-# PRODUCT_BUILD_PROP_OVERRIDES += \
-#     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
-#     PRODUCT_PRODUCT="beryllium"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    PRODUCT_PRODUCT="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
